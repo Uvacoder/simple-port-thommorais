3b8c4ab7dd159533a0abf6eaae2cc5ae95cf4b08
thommorais <thom@nitrodev.com>
2017-04-13 06:57:19 -0300

    as

--- json/main.js
+++ json/main.js
@@ -5,7 +5,7 @@ function loadJSON(file) {
         var xobj = new XMLHttpRequest()
         xobj.overrideMimeType("application/json")
         xobj.open('GET', file, true)
-        xobj.onreadystatechange = function () {
+        xobj.onreadystatechange = () => {
 
             if (xobj.readyState == 4 && xobj.status == "200") {
                 if (xobj.status == 200) resolve(JSON.parse(xobj.responseText))
@@ -21,9 +21,46 @@ function loadJSON(file) {
 // shortcut to select DOM elements
 var d = document.querySelector.bind(document)
 
+
+
+// caching the article item template
+var newsTemplate        = d('#newsItem'),
+            itemfig     = newsTemplate.content.querySelector('figure'),
+            itemThumb   = newsTemplate.content.querySelector('figure img'),
+            item        = newsTemplate.content.querySelector('article'),
+            itemTitle   = newsTemplate.content.querySelector('h2'),
+            itemHat     = newsTemplate.content.querySelector('h3'),
+            itemDate    = newsTemplate.content.querySelector('time span')
+
+if(news_json){
+    loadJSON(news_json)
+        .then(response => {
+            let list = response.data.campos.lista
+
+            Object.keys(list).forEach( i => {
+                
+                if(list[i].campos.template.thumb_url)
+                    itemThumb.setAttribute('src', list[i].campos.template.thumb_url)
+
+                
+                itemTitle.innerHTML   =  list[i].campos.info.titulo
+                itemHat.innerHTML     =  list[i].campos.info.apoio
+                itemDate.innerHTML    =  list[i].campos.info.hora
+
+
+                clone = document.importNode(newsTemplate.content, true)
+                document.querySelector('body').appendChild(clone)
+
+            })
+
+        })
+}
+
+
+
 // caching the article template
 var clone = '',
-    articleTemplate  = d('#articleJson'),
+    articleTemplate = d('#articleJson'),
             article = articleTemplate.content.querySelector('article'),
             titule  = articleTemplate.content.querySelector('h1'),
             hat     = articleTemplate.content.querySelector('h3'),
@@ -31,46 +68,20 @@ var clone = '',
             subject = articleTemplate.content.querySelector('.assunto'),
             content = articleTemplate.content.querySelector('.article-wrp')
 
-loadJSON(article_json)
-    .then(response => {
+if(article_json){
+    loadJSON(article_json)
+        .then(response => {
 
-        let art = response.data
+            let art = response.data
 
-        titule.innerHTML  =  art.titulo
-        hat.innerHTML     =  art.chapeu
-        date.innerHTML    =  art.dt_publi
-        subject.innerHTML =  art.assunto
-        content.innerHTML =  art.conteudo
+            titule.innerHTML  =  art.titulo
+            hat.innerHTML     =  art.chapeu
+            date.innerHTML    =  art.dt_publi
+            subject.innerHTML =  art.assunto
+            content.innerHTML =  art.conteudo
 
-        clone = document.importNode(articleTemplate.content, true)
-        document.querySelector('body').appendChild(clone)
-
-    })
-
-// caching the article item template
-
-var newsTemplate        = d('#newsItem'),
-            item        = newsTemplate.content.querySelector('article'),
-            itemTitle   = newsTemplate.content.querySelector('h2'),
-            itemHat     = newsTemplate.content.querySelector('h3'),
-            itemDate    = newsTemplate.content.querySelector('time span')
-
-loadJSON(news_json)
-    .then(response => {
-        let list = response.data.campos.lista
-
-        Object.keys(list).forEach( i => {
-            
-            console.log(list[i].campos.info)
-
-            itemTitle.innerHTML   =  list[i].campos.info.titulo
-            itemHat.innerHTML     =  list[i].campos.info.apoio
-            itemDate.innerHTML    =  list[i].campos.info.hora
-
-
-            clone = document.importNode(newsTemplate.content, true)
+            clone = document.importNode(articleTemplate.content, true)
             document.querySelector('body').appendChild(clone)
 
         })
-
-    })
+}
