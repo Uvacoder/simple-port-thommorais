7fa4e07e60099206897e1ca36c97fb1731410992
thom <thommorais@gmail.com>
2017-06-02 09:44:45 -0300

    cleaning this up

---
+++ search/assets/js/z_/swipper.js
@@ -0,0 +1,11 @@
+const places = new Swiper('#places', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    slidesPerView: 2,
+    spaceBetween: 10
+})
+
+const gallerie = new Swiper('#gallerie', {
+    slidesPerView: 'auto',
+    spaceBetween: 10
+})
