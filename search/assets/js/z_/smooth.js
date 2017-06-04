ced146c93ee4e3c6a4116cd20343f3b969a935f2
thom <thommorais@gmail.com>
2017-06-04 18:29:41 -0300

    update

---
+++ search/assets/js/z_/smooth.js
@@ -0,0 +1,9 @@
+// activing the smoothScroll Plugin
+SmoothScroll({
+    // Acceleration
+    accelerationDelta: 50,  // 50
+    accelerationMax: 3,   // 3
+    fixedBackground   : true, 
+    touchpadSupport   : true, // ignore touchpad by default
+})
+
--- search/assets/js/z_/swipper.js
+++ search/assets/js/z_/swipper.js
@@ -2,28 +2,10 @@ const places = new Swiper('#places', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
     slidesPerView: 2,
-    spaceBetween: 10,
-    breakpoints: {
-        480: {
-            slidesPerView: 1,
-            spaceBetween: 10
-        },
-
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 10
-        }
-    }
+    spaceBetween: 10
 })
 
 const gallerie = new Swiper('#gallerie', {
     slidesPerView: 'auto',
-    spaceBetween: 10,
-
-    breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 10
-        }
-    }
+    spaceBetween: 10
 })
