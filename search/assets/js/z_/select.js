094e336bb3f4e2ede980bfe7379326352063e8fc
thom <thommorais@gmail.com>
2017-06-04 21:13:25 -0300

    update

--- search/assets/js/z_/select.js
+++ search/assets/js/z_/select.js
@@ -2,11 +2,8 @@ const select = document.querySelector('#select') || false
 const input = document.querySelector('#search-input')
 
 function changeSearch(){
-   
-    parallaxEl.style.backgroundImage = `url('assets/images/${select.value}.png')`
-
-    input.setAttribute('placeholder', placeholder[select.value])
-
+    parallaxEl.style.backgroundImage = placeholder[select.value].url
+    input.setAttribute('placeholder', placeholder[select.value].placeholder)
 }
 
 select && select.addEventListener('change', changeSearch) || changeSearch()
